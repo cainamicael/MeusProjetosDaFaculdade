a37e81a2de2b67c2c0e27dd19d34a8dadd7852ff
cainamicael <cainamicaeloficial@gmail.com>
2022-05-19 15:43:20 -0300

    Space

--- Java/Encapsulamento/ExPessoalCelularPoo/src/samsung/Touch.java
+++ Java/Encapsulamento/ExPessoalCelularPoo/src/samsung/Touch.java
@@ -4,12 +4,16 @@ public interface Touch {
 
     public abstract void ligar();
     public abstract void desligar();
+
     public abstract void desbloquear();
     public abstract void bloquear();
+
     public abstract void abrirWhatsapp();
     public abstract void fecharWhatsapp();
+
     public abstract void abrirInstagram();
     public abstract void fecharInstagram();
+    
     public abstract void status();
     public abstract void statusLogico();
 
